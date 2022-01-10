dede1022dc717660c26d5bac782da057d6c24a4d
IgorShum <Shumeikoigor@yandex.ru>
2022-01-10 14:32:15 +0300

    + bootstrap + vue:progress-bar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
-  <div class="container"> {{ a }}</div>
+  <div>
+    <button class="btn btn-primary" v-on:click="showRes = !showRes"> {{ btnText }} </button>
+    <hr>
+    {{ progress }}
+    <hr>
+    <transition name="slide">
+      <h2 v-show="showRes" class="alert alert-success">
+        Profit: {{ sum }}
+      </h2>
+    </transition>
+    <hr>
+    <app-progress v-bind:val="numbers.length" v-bind:max="maxNumbers">
+    </app-progress>
+    <hr>
+    <button class="btn btn-success"
+                  v-on:click="addNumber"
+                  v-bind:disabled="done"
+                  >
+                  Add number
+    </button>
+    <hr>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="number in numbers" :key="number">
+      {{ number }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import progress from './components/ProgressBar.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    AppProgress: progress
   },
   data(){
     return {
-      a: 2
+          showRes: true,
+          numbers: [],
+          maxNumbers: 10
+        }
+  },
+  methods: {
+    addNumber(){
+      if(!this.done){
+        let rnd = Math.floor(Math.random() * 11) - 5;
+        this.numbers.push(rnd);
+      }
+    }
+  },
+  computed: {
+    sum(){
+      let sum = 0;
+
+      for(let i = 0; i < this.numbers.length; i++){
+        sum += this.numbers[i];
+      }
+      return sum;
+    },
+    btnText(){
+      return this.showH2 ? 'Hide result' : 'Show result';
+    },
+    done(){
+      return this.numbers.length >= this.maxNumbers;
     }
   }
 }
@@ -26,4 +79,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.slide-enter {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  transition: opacity 0.5s;
+}
+
+.slide-leave-active {
+  transition: opacity 0.5s;
+}
+
+.slide-leave-to {
+  opacity: 0;
+}
+
 </style>
